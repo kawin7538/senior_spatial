162477d5b0e028278f84b394f6c20fb7d15a5705
Kawin Chinpong <kawin7538@hotmail.com>
2021-10-09 11:19:48 +0700

    Add overall data function for 4th perspective

--- data_loading/distribution_data.py
+++ data_loading/distribution_data.py
@@ -1,4 +1,7 @@
+import gc
+
 import pandas as pd
+
 
 class Distribution_Data:
     def __init__(self,load_ratio=True,range_year=range(2011,2021),**kwargs) -> None:
@@ -12,18 +15,32 @@ class Distribution_Data:
             f'preprocessed_data/DHF/monthly{self.ratio_keyword}',
             f'preprocessed_data/DSS/monthly{self.ratio_keyword}',
         ]
-        self.list_type_keyword=['DF','DHF','DSS']
+        self.base_output_path=self._config_base_output_path(load_ratio=load_ratio)
+        self.list_type_keyword=['DF','DHF','DSS','ALL']
         self.list_case_df=self._custom_edit_data(self._read_csv_list(data_keyword='case'))
         # self.list_case_minmax_value=self._get_list_minmax_value(data_keyword='case')
         self.list_death_df=self._custom_edit_data(self._read_csv_list(data_keyword='death'))
         # self.list_death_minmax_value=self._get_list_minmax_value(data_keyword='death')
         print("\tDistribution Data Loaded---")
 
+    def _config_base_output_path(self,load_ratio):
+        return 'output/ratio/'if load_ratio else 'output/raw/'
+
     def _read_csv_list(self,data_keyword='case'):
         list_df=[]
         for keyword,path in zip(self.list_type_keyword,self.list_path):
             temp_path=f'{path}/all{data_keyword}{self.ratio_keyword if self.ratio_keyword=="_ratio" else "_mcd"}_{keyword}{"" if self.ratio_keyword=="_ratio" else "_"}.csv'
             list_df.append(pd.read_csv(temp_path))
+        temp_df=list_df[0].copy()
+
+        for i in range(1,len(list_df)):
+            temp_df+=list_df[i]
+        temp_df['area']=list_df[0]['area']
+        list_df.append(temp_df)
+
+        del temp_df
+        gc.collect()
+
         return list_df
 
     def _custom_edit_data(self,list_df):
