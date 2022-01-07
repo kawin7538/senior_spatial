b386643f07e2d5c41022851fbd87ea63b8174b8a
Kawin Chinpong <kawin7538@hotmail.com>
2022-01-07 10:55:29 +0700

    update testcase display in real time with turbo-flask

--- testcase_display.py
+++ testcase_display.py
@@ -1,15 +1,20 @@
 import os
 import pandas as pd
 from flask import Flask, render_template, url_for, send_from_directory
+from turbo_flask import Turbo
+import threading
+import time
 
 app = Flask(__name__,static_folder='output/testcase_output', static_url_path='')
+turbo = Turbo(app)
 
 @app.route("/")
 def main():
-    list_allcase=sorted(set([i.split('.')[0] for i in os.listdir("output/testcase_output")]),key=lambda x: (int(x.split('_')[0]),-int(x.split('_precision')[-1])))
-    list_casenumber=[i.split('_')[0] for i in list_allcase]
-    list_precision=[i.split('_precision')[-1] for i in list_allcase]
-    return render_template("index.html",list_allcase=list_allcase,list_casenumber=list_casenumber,list_precision=list_precision)
+    # list_allcase=sorted(set([i.split('.')[0] for i in os.listdir("output/testcase_output")]),key=lambda x: (int(x.split('_')[0]),-int(x.split('_precision')[-1])))
+    # list_casenumber=[i.split('_')[0] for i in list_allcase]
+    # list_precision=[i.split('_precision')[-1] for i in list_allcase]
+    # return render_template("index.html",list_allcase=list_allcase,list_casenumber=list_casenumber,list_precision=list_precision)
+    return render_template("index.html")
 
 @app.route("/detail/<int:testcase>/<int:precision>")
 def detail(testcase,precision):
@@ -27,6 +32,23 @@ def detail(testcase,precision):
     c_global_table=pd.read_csv("output/testcase_output/"+target_case+".c.global.csv").to_html(classes=["table","table-striped","table-hover"],index=False)
     c_local_table=pd.read_csv("output/testcase_output/"+target_case+".c.local.csv").to_html(classes=["table","table-striped","table-hover"],index=False)
     return render_template("detail.html",testcase=testcase,precision=precision,dist_table=dist_table,gi_table=gi_table,g_global_table=g_global_table,gistar_table=gistar_table,moran_global_table=moran_global_table,lisa_table=lisa_table,c_global_table=c_global_table,c_local_table=c_local_table)
+
+@app.context_processor
+def inject_load():
+    list_allcase=sorted(set([i.split('.')[0] for i in os.listdir("output/testcase_output")]),key=lambda x: (int(x.split('_')[0]),-int(x.split('_precision')[-1])))
+    list_casenumber=[i.split('_')[0] for i in list_allcase]
+    list_precision=[i.split('_precision')[-1] for i in list_allcase]    
+    return {'list_allcase':list_allcase,'list_casenumber':list_casenumber,'list_precision':list_precision}
+
+@app.before_first_request
+def before_first_request():
+    threading.Thread(target=update_load).start()
+
+def update_load():
+    with app.app_context():
+        while True:
+            time.sleep(5)
+            turbo.push(turbo.update(render_template('index.html'), 'load'))
 
 if __name__ == "__main__":
     app.run(debug=True)
