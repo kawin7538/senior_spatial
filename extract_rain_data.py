83a0a5018b87e98628737ef7754809980e5bdf50
Kawin Chinpong <kawin7538@hotmail.com>
2022-02-10 16:53:56 +0700

    update rain extraction

--- extract_rain_data.py
+++ extract_rain_data.py
@@ -40,23 +40,6 @@ def export_df(list_links,keyword1,keyword2):
 if __name__=='__main__':
     html=get_html(url)
     list_links=get_all_items(html)
-    # total_rain_df=pd.DataFrame()
-
-    # for link in list_links:
-    #     print(link.split('/')[1])
-    #     df=pd.read_excel(f"http://service.nso.go.th/nso/web/statseries/{link}",skiprows=[0,1,2,3,5],skipfooter=4,header=0)
-    #     df=df.dropna()
-    #     new_col=list(range(2003,2016))
-    #     df.columns=[list(df.columns)[0]]+new_col+[list(df.columns)[-1]]
-    #     new_df=pd.DataFrame(columns=new_col)
-    #     new_df=new_df.append(df.loc[df['Item']=='Total rain (millimeter)'].iloc[:,1:14])
-    #     new_df=new_df.assign(Area=' '.join(link.split('/')[1].split('_')[1:])).reset_index(drop=True)
-    #     total_rain_df=total_rain_df.append(new_df)
-
-    # total_rain_df=total_rain_df.replace('-',np.nan).replace('-   ',np.nan)
-    # # print(total_rain_df.fillna(0).info())
-    # total_rain_df.to_csv("preprocessed_data/total_rain.csv",index=False)
-    # total_rain_df.fillna(0).groupby('Area').agg('mean').reset_index().replace(0,np.nan).to_csv("preprocessed_data/mean_total_rain.csv",index=False)
 
     export_df(list_links,"Total rain (millimeter)","total_rain")
     export_df(list_links,"Number of rainy days (day)","cnt_day_rain")
