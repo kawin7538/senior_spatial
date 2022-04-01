bca8bb1267a4b14f99d52b096e53010c1546b9f2
Kawin Chinpong <kawin7538@hotmail.com>
2022-04-01 04:36:46 +0000

    update besag

---
+++ R_sources/_test_playcase2_source.r
@@ -0,0 +1,70 @@
+library(dplyr)
+library(sp)
+library(rgdal)
+library(RColorBrewer)
+library(geodata)
+library(spdep)
+library(INLA)
+
+pop.df<-read.csv("testcase_modules/DF_case_mean_allyears.csv",stringsAsFactors=FALSE)
+
+thai_map2<-geodata::gadm(country="Thailand",level=1,path='preprocessed_data')
+adj_list=adjacent(thai_map2[order(thai_map2$NAME_1),],type='queen',symmetrical=TRUE)
+# add pair "Phung Nga" and "Phuket"
+adj_list<-rbind(adj_list,c(39,48))
+adj_mat<-matrix(0,77,77)
+adj_mat[adj_list]<-1
+adj_mat[adj_list[,c(2,1)]]<-1
+W<-mat2listw(adj_mat,style='B')
+
+formula<-total~1+f(ID,model="besag",graph=listw2mat(W))
+
+run_besag <- function(test_df){
+    test_df['ID']<-1:nrow(test_df)
+    test_df['E']<-pop.df['total']*100000
+    test_df['total_raw']<-test_df['total']
+    test_df$total<-as.integer(with(test_df,total*E))
+
+    thai_map2.corrected<-merge(thai_map2,test_df,by='NAME_1')
+
+    custommodel.besag<-inla(
+        formula,
+        data=test_df,
+        family="poisson",
+        E=E,
+        control.compute=list(dic=TRUE, waic=TRUE, cpo=TRUE,return.marginals.predictor=TRUE),
+        control.predictor=list(compute=TRUE)
+    )
+
+    thai_map2.corrected[,'besag']<-(custommodel.besag$summary.fitted.values$mean)
+    thai_map2.corrected[,'besag_E']<-(custommodel.besag$summary.fitted.values$mean*test_df['E'])
+
+    # spplot(thai_map2.corrected,"total_raw",do.log=TRUE,scales=list(draw=TRUE))
+
+    # spplot(thai_map2.corrected,"besag",do.log=TRUE,scales=list(draw=TRUE))
+
+    # plot(thai_map2.corrected$total,thai_map2.corrected$besag_E)
+    # abline(0,1)
+
+    marginal<-custommodel.besag$marginals.fitted.values$fitted.Predictor.72
+    marginal<-inla.smarginal(marginal)
+    marginal<-data.frame(marginal)
+    # library(ggplot2)
+    # ggplot(marginal, aes(x = x, y = y)) + geom_line() +
+    # labs(x = expression(beta[1]), y = "Density") +
+    # geom_vline(xintercept = 0, col = "black") + theme_bw()
+
+    marginal<-custommodel.besag$marginals.fitted.values
+    exc <- sapply(custommodel.besag$marginals.fitted.values,
+                FUN = function(marg){1-inla.pmarginal(q=2, marginal = marg)})
+    # thai_map2.corrected[,'exc']<-exc
+
+    # spplot(thai_map2.corrected,"exc",do.log=TRUE,scales=list(draw=TRUE))
+    # print(thai_map2.corrected)
+    # return(exc>0.95)
+
+    test_df['exc']<-exc
+    test_df['cl']<-exc>0.95
+
+    return(test_df)
+}
--- test_rpy2.py
+++ test_rpy2.py
@@ -1,4 +1,10 @@
+import numpy as np
+import pandas as pd
 import rpy2.robjects as robjects
+from rpy2.robjects import pandas2ri
+
+from rpy2.robjects.conversion import localconverter
+
 r = robjects.r
 r['source']('R_sources/_test_rpy2_source.r')
 
@@ -6,4 +12,15 @@ x=[1,2,3,4,5]
 y=[2,4,6,8,10]
 
 r_func1=robjects.globalenv['func1']
-r_func1(x,y)
+
+df=pd.read_csv("output/testcase_output2/dataframe_sim_output/m2.csv")[['NAME_1','total_112']].rename(columns={f'total_112':'total'})
+
+with localconverter(robjects.default_converter + pandas2ri.converter):
+  r_from_pd_df = robjects.conversion.py2rpy(df)
+
+ans_df_r=r_func1(r_from_pd_df)
+
+with localconverter(robjects.default_converter + pandas2ri.converter):
+  ans_df = robjects.conversion.rpy2py(ans_df_r)
+
+print(ans_df)
