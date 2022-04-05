961c9d4b867b4dc3402b4b963af0577fa4018d4e
Kawin Chinpong <kawin7538@hotmail.com>
2022-04-05 10:18:18 +0000

    update satscan procedure

--- R_sources/_test_playcase2_source.r
+++ R_sources/_test_playcase2_source.r
@@ -5,6 +5,7 @@ library(RColorBrewer)
 library(geodata)
 library(spdep)
 library(INLA)
+library(rsatscan)
 
 pop.df<-read.csv("testcase_modules/DF_case_mean_allyears.csv",stringsAsFactors=FALSE)
 
@@ -16,6 +17,7 @@ adj_mat<-matrix(0,77,77)
 adj_mat[adj_list]<-1
 adj_mat[adj_list[,c(2,1)]]<-1
 W<-mat2listw(adj_mat,style='B')
+centroid<-centroids(thai_map2)
 
 run_besag <- function(test_df){
     test_df['ID']<-1:nrow(test_df)
@@ -41,7 +43,7 @@ run_besag <- function(test_df){
 
     marginal<-custommodel.besag$marginals.fitted.values
     exc <- sapply(custommodel.besag$marginals.fitted.values,
-                FUN = function(marg){1-inla.pmarginal(q=3, marginal = marg)})
+                FUN = function(marg){1-inla.pmarginal(q=1, marginal = marg)})
 
     test_df['exc']<-exc
     test_df['cl']<-exc>0.95
@@ -73,10 +75,50 @@ run_bym <- function(test_df){
 
     marginal<-custommodel.besag$marginals.fitted.values
     exc <- sapply(custommodel.besag$marginals.fitted.values,
-                FUN = function(marg){1-inla.pmarginal(q=3, marginal = marg)})
+                FUN = function(marg){1-inla.pmarginal(q=1, marginal = marg)})
 
     test_df['exc']<-exc
     test_df['cl']<-exc>0.95
+
+    return(test_df)
+}
+
+run_satscan <- function(test_df){
+    test_df['ID']<-1:nrow(test_df)
+    test_df['E']<-pop.df['total']*100000
+    test_df['total_raw']<-test_df['total']
+    test_df['year']<-1
+    test_df$total<-as.integer(with(test_df,total*E))
+
+    thai_map2.corrected<-merge(thai_map2,test_df,by='NAME_1')
+
+    test_df[,c('x','y')]<-geom(centroid)[,c('x','y')]
+    case.df<-test_df[,c('ID','total')]
+    case.df<-case.df%>%rename(cases=total)
+    pop.df<-test_df[,c('ID','year','E')]
+    pop.df<-pop.df%>%rename(population=E)
+    geo.df<-test_df[,c('ID','x','y')]
+
+    td = tempdir()
+    write.cas(case.df, td,"Testcase")
+    write.geo(geo.df, td,"Testcase")
+    write.pop(pop.df, td,"Testcase")
+    invisible(ss.options(reset=TRUE))
+    ss.options(list(CaseFile="Testcase.cas", PrecisionCaseTimes=0,
+                    PopulationFile="Testcase.pop",
+                    CoordinatesFile="Testcase.geo", CoordinatesType=0, AnalysisType=1,
+                    ModelType=0, ScanAreas=1, TimeAggregationUnits=0, MaxSpatialSizeInPopulationAtRisk_Reported=25,MaxSizeInMaxCirclePopulationFile_Reported=25))
+    ss.options(c("NonCompactnessPenalty=0", "ReportGiniClusters=n", "LogRunToHistoryFile=n"))
+    write.ss.prm(td,"Testcase")
+    testcase_satscan = satscan(td,"Testcase",sslocation="satscan_sources",ssbatchfilename="satscan_stdc++6_x86_64_64bit",verbose = FALSE)
+    
+    cluster.center<-testcase_satscan$col
+    hotspot<-testcase_satscan$gis
+
+    hotspot.tmp<-hotspot
+    hotspot.tmp<-as.integer(as.character(hotspot.tmp[hotspot.tmp$P_VALUE<0.05,"LOC_ID"]))
+    test_df[,'cl']<-FALSE
+    test_df[hotspot.tmp,'cl']<-TRUE
 
     return(test_df)
 }
